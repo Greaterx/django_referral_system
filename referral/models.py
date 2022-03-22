cb2860607e400f82b628491d5f5a707fbf23807d
Ivan Volontirec <ferdfred123@gmail.com>
2022-03-22 16:26:37 +0300

    second commit

--- referral/models.py
+++ referral/models.py
@@ -6,4 +6,4 @@ class Referral(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     my_referral = models.CharField(max_length=6)
     another_referral = models.CharField(max_length=6)
-    phone_number = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=12)
